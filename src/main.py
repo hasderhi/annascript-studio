0b317de55438f07710b909a90b16efae7f3690c9
Annabeth Kisling <hasederhi.12@gmail.com>
2026-09-12 16:16:17 +0200

    update settings entries;

--- src/main.py
+++ src/main.py
@@ -89,13 +89,16 @@ LOCAL_ASSET_EXTENSIONS = {
 
 SETTINGS = {
     "General": {
-        "author-name": (None, "Default author name", "text", None, ""),
+        "author-name": (None, "Default Author Name", "text", None, ""),
         "default-theme": (None, "Default Theme", "dropdown", ["Accessible", "Cyberpunk", "Default", "Essay", "Essay-Pro", "Essay-Print", "Forest", "Notes", "Pastel", "Retro", "Solarized"], "Default"),
-        "start-with-darkmode-as-default": (None, "Start documents in darkmode", "checkbox", None, True),
-        "default-path": (None, "Default file path", "text", None, ""),
+        "default-path": (None, "Default File Path", "text", None, ""),
+        "start-with-darkmode-as-default": (None, "Start Documents in Darkmode", "checkbox", None, False),
     },
     "View": {
         "font-size": (None, "Editor Font Size", "spinbox", [1, 200], 14),
+    },
+    "Advanced": {
+        "update-check": (None, "Check for Updates", "checkbox", None, True),
     }
 }
 
